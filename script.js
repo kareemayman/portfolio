bf42f944840fb1071c320fe9a8a1d0cef4a0c041
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-11-21 13:00:06 +0200

    Added Scroll To Top Button

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const skillSection = document.getElementById('skills')
 const skills = Array.from(document.querySelectorAll('.skills .flex div div'))
 
+const scrollToTopButton = document.querySelector('.scroll-to-top-button')
+
+// Scroll Event
 window.addEventListener('scroll', e => {
 
+    // Get Scroll Position
     const scrollPos = window.scrollY
 
-    if (scrollPos >= (skillSection.offsetTop - 50)) {
+    // For Scroll To Top Button
+    if (scrollPos >= 250) {
+        
+        scrollToTopButton.style.opacity = 1
+    } else {
+
+        scrollToTopButton.style.opacity = 0
+    }
+
+    // For Skills Progress Bar
+    if (scrollPos >= (skillSection.offsetTop - 300)) {
 
         skills.forEach(skill => {
             
@@ -13,4 +27,13 @@ window.addEventListener('scroll', e => {
             span.style.width = skill.dataset.percent
         })
     }
+})
+
+// Event For Clicking On Scroll To Top Button
+scrollToTopButton.addEventListener('click', e => {
+
+    window.scrollTo({
+        behavior: "smooth",
+        top: 0  
+    })
 })
